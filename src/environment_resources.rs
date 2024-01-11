da88d81927d6db181fee28f7bd34d5f0ac3173e5
hkohko <hekodev.py@gmail.com>
2024-01-11 09:54:08 +0700

    documented new additions to the game

--- src/environment_resources.rs
+++ src/environment_resources.rs
@@ -1,19 +1,23 @@
 use crate::{Resources, Coordinates};
 #[derive(Debug)]
+/// Struct for resources available in the environment.
 pub struct EnvResource {
     kind: Resources,
     coordinates: Coordinates,
 }
 impl EnvResource {
+    /// Creates a new resource with randomized values.
     pub fn new(at_most: i32) -> EnvResource {
         EnvResource {
             kind: Resources::randomize(at_most),
             coordinates: Coordinates::randomize(),
         }
     }
+    /// Returns the resource's kind.
     pub fn get_kind(&self) -> Resources {
         self.kind
     }
+    /// Returns the resource's coordinates.
     pub fn get_coordinates(&self) -> Coordinates {
         self.coordinates
     }
